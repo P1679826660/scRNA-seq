ed612776e7c1a33c04370461b56a30b57f014f6b
P1679826660 <1679826660@qq.com>
2026-01-29 16:58:08 +0800

    Update 主代码.R

--- 主代码.R
+++ 主代码.R
@@ -399,6 +399,17 @@ print("全部分析流程结束！")
 
 
 
+##########################
+#pbmc是大的图谱   pbmc$celltype.1
+              #TA是pbmc的亚群pbmc$celltype
+
+library(dplyr)
+pbmc@meta.data <- pbmc@meta.data %>%
+  mutate(celltype.2 = coalesce(TA@meta.data$celltype[match(rownames(pbmc@meta.data), rownames(TA@meta.data))], 
+                                                             celltype.1))
+
+
+
 
 
 
